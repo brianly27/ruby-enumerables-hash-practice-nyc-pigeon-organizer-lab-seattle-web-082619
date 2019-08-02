63e4e39c0b09229db297e1d6924c996cab6a9f74
Brian Ly <brian.kyang.ly@gmail.com>
2019-08-02 23:26:05 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -19,6 +19,7 @@ def nyc_pigeon_organizer(data)
       memo
     end #data.reduce(att_hash)
 
+<<<<<<< HEAD
     data.each do |bird_attribute, name_array| #(:color, {:purple => ["Theo", "Peter Jr.", "Lucky"]})
       name_array.each do |attribute, bird_names| #(:purple, ["Theo", "Peter Jr.", "Lucky"])
         if bird_names.include?(name)
@@ -28,6 +29,22 @@ def nyc_pigeon_organizer(data)
     end #data.each
   #assign att_hash to name
     nyc[name] = att_hash
+=======
+    data.each do |bird_attribute|
+      bird_attribute.reduce(att_hash) do |memo, (attribute, value)|
+
+
+          att_hash[attribute] <<  value.find {|a| a == name}
+          memo
+
+        binding.pry
+      end #bird_attribute.reduce
+    end #data.each
+
+    #assign att_hash to name
+    nyc[name] = att_hash
+
+>>>>>>> e3ced6a959ad81452a43daa8591a6e1f8d92f8a5
   end #names.each
   nyc
 end
